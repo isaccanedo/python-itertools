800d12740b3000d07f2f335ba6340a6c13050298
Isac Canedo <32867124+isaccanedo@users.noreply.github.com>
2024-06-01 14:22:40 -0300

    Update combin.py

--- combin.py
+++ combin.py
@@ -6,6 +6,11 @@ conjunto = [1, 2, 3, 4, 5]
 # Gerar todas as combinações de 2 elementos
 combinacoes = list(itertools.combinations(conjunto, 2))
 
+# Calcular e imprimir a quantidade de combinações geradas
+quantidade_combinacoes = len(combinacoes)
+print("Quantidade de combinações geradas:", quantidade_combinacoes)
+
 # Imprimir as combinações
 for combinacao in combinacoes:
     print(combinacao)
+
